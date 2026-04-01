0f6408b12b4f185d9ee2563f7c972e9898a62e18
tommorvolloriddle <96335548+tommorvolloriddle@users.noreply.github.com>
2026-04-01 11:34:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function acceptLove() {
   document.getElementById("result").innerHTML =
-    "✅ Consent Signed! Trial Approved! Now the study 'US_FOREVER' is officially ongoing ❤️💍";
+    "✅ Consent Signed! Trial Approved! Now the study 'AN ❤️ SA' is officially ongoing ❤️💍";
 }
