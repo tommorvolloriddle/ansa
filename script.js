4fdadfe3f8590a0a023a7976682caa5bc2f2bb68
tommorvolloriddle <96335548+tommorvolloriddle@users.noreply.github.com>
2026-04-01 11:26:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function acceptLove() {
   document.getElementById("result").innerHTML =
-    "🎉 Merge Successful! Now our hearts are in the same repository forever ❤️💍";
+    "✅ Consent Signed! Trial Approved! Now the study 'US_FOREVER' is officially ongoing ❤️💍";
 }
